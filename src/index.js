875b79425906d476e39b9861f8bd822d4461ba88
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-01-12 16:32:51 +0530

    models(schema) created

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 import app from "./app.js";
 import connectDB from "./config/db.js";
+import User from "./models/User.js";
 
 const PORT = process.env.PORT;
 
---
+++ src/models/User.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+// ✅ CORRECT pre-save middleware
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Password compare method
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
