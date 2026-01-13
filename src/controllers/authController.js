287ce9651e5f0bdf4d4944adbeb012bea130062f
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-01-13 17:32:57 +0530

    cookie changes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -30,8 +30,8 @@ export const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
