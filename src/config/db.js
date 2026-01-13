953034223feb71e77efeb30e4b0a36ff4d9cf065
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-01-13 17:42:47 +0530

    database config changes for vercel

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,39 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.DATABASE_URL);
+//     console.log("MongoDB connected");
+//   } catch (error) {
+//     console.error("MongoDB connection failed");
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
+
+
 import mongoose from "mongoose";
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DATABASE_URL);
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("MongoDB connection failed");
-    process.exit(1);
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.DATABASE_URL, {
+      bufferCommands: false,
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 export default connectDB;
